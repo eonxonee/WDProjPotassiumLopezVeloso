d7e0c8b346b96a196c07a42e85ca40dd4d271dba
Hannah-Malaika-Lopez <b2029hmblopez@pshs.edu.ph>
2025-12-09 12:05:14 +0800

    new

--- main files/main.js
+++ main files/main.js
@@ -1,14 +1,22 @@
-function clock(){
+    function clock(){
     const currentTime = new Date();
     var hours= currentTime.getHours();
     var minutes= currentTime.getMinutes();
     var seconds= currentTime.getSeconds();
     seconds=addZero(seconds);
-    minute=addZero(minutes);
+    minutes=addZero(minutes);
     document.getElementById("time").innerHTML = hours +":"+ minutes +":"+ seconds;
     setTimeout(clock,1000);
+    return hours;
 }
 function addZero(i){
     if(i<10){i="0"+i}
     return i;
+}
+function dayYear(){
+    const currentDate =new Date();
+    var month=currentDate.getMonth()+1;
+    var day=currentDate.getDate();
+    var year=currentDate.getFullYear()-6;
+    document.getElementById("date").innerHTML = month+"/"+day+"/"+year;
 }
