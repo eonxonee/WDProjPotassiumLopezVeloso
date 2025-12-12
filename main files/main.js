1553ac74a698fd1ccf38e60cf1cff26842c68e0a
Hannah-Malaika-Lopez <b2029hmblopez@pshs.edu.ph>
2025-12-12 17:15:14 +0800

    notdone

--- main files/main.js
+++ main files/main.js
@@ -7,12 +7,16 @@
     minutes=addZero(minutes);
     document.getElementById("time").innerHTML = hours +":"+ minutes +":"+ seconds;
     setTimeout(clock,1000);
+
+    let day=Number(document.getElementById("day").innerHTML);
+    let dayCounter=Number(document.getElementById("dayCounter").innerHTML)
+    if(hours==14 && minutes==26 && seconds==30){
+        document.getElementById("day").innerHTML= day+1;
+        document.getElementById("dayCounter").innerHTML= dayCounter+1;
+    }
+    
 }
 function addZero(i){
     if(i<10){i="0"+i}
     return i;
-}
-function day(){
-    document.getElementById("day").innerHTML= "1";
-    document.getElementById("date").innerHTML= "18";
 }
