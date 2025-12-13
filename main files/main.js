7c3c9ffaa8485495d4e815c6fb32bc84738a6d69
Hannah-Malaika-Lopez <b2029hmblopez@pshs.edu.ph>
2025-12-13 22:24:41 +0800

    added intro

--- main files/main.js
+++ main files/main.js
@@ -10,7 +10,7 @@
 
     let day=Number(document.getElementById("day").innerHTML);
     let dayCounter=Number(document.getElementById("dayCounter").innerHTML)
-    if(hours==14 && minutes==26 && seconds==30){
+    if(hours==12 && minutes==0 && seconds==0){
         document.getElementById("day").innerHTML= day+1;
         document.getElementById("dayCounter").innerHTML= dayCounter+1;
     }
@@ -19,4 +19,16 @@
 function addZero(i){
     if(i<10){i="0"+i}
     return i;
+}
+let openingScreen=document.getElementById("openingScreen");
+let titleScreen=document.getElementById("titleScreen");
+let caseFile=document.getElementById("caseFile");
+
+openingScreen.addEventListener("click",start);
+
+function start(){
+    openingScreen.classList.add("titleScreenFade");
+    titleScreen.classList.add("titleScreenHide");
+    caseFile.classList.add("caseFileEnter");
+
 }
