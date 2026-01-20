735c3d81fac52b4bbb5dee830efe85332a6ca49e
Hannah-Malaika-Lopez <b2029hmblopez@pshs.edu.ph>
2026-01-20 21:10:09 +0800

    added notes base

--- main files/main.js
+++ main files/main.js
@@ -5,3 +5,14 @@ function footerToggle(){
     document.getElementById("mask").classList.toggle("cover");
     
 }
+function footerClose(){
+    document.getElementById("toggleFooter").classList.remove("open");
+    document.getElementById("footer").classList.remove("reveal");
+    document.getElementById("mask").classList.remove("cover");
+}
+function noteSave(){
+    let newNote=document.getElementById("notesInput").value;
+    localStorage.setItem("savedNote", newNote);
+    let x=localStorage.getItem("savedNote");
+    console.log(x);
+}
