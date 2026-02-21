6663bc931780142f9ba2ef5708c4c4d234de34ca
Hannah-Malaika-Lopez <b2029hmblopez@pshs.edu.ph>
2026-02-21 10:27:37 +0800

    css milestone

--- main files/main.js
+++ main files/main.js
@@ -1,3 +1,6 @@
+let notes=[];
+let uniqueTitle=[];
+let previewNotes=document.getElementById('previewNotes');
 //onload
 function loaded(){
     renderNotes();
@@ -56,9 +59,8 @@ function closeNotes(){
 }
 
 //notes main functions
-const notes=JSON.parse(localStorage.getItem('allNotes')) || [];
-let previewNotes=document.getElementById('previewNotes');
-const uniqueTitle=JSON.parse(localStorage.getItem('uniqueTitle')) || [];
+notes=JSON.parse(localStorage.getItem('allNotes')) || [];
+uniqueTitle=JSON.parse(localStorage.getItem('uniqueTitle')) || [];
 
 function saved(event){
     event.preventDefault();
@@ -66,8 +68,20 @@ function saved(event){
     let notesContent=document.getElementById('notesContent').value;
 
     if(uniqueTitle.includes(notesTitle)){
-        alert("Title used");
-        return;
+        let check=confirm(`Save changes to ${notesTitle}?`);
+        if(check==true){
+            let savedNotes=JSON.parse(localStorage.getItem('allNotes'));
+        for(let i in savedNotes){
+        for(let key in savedNotes[i]){
+            if(savedNotes[i][key]==notesTitle){
+                notes[i]['content']=notesContent;
+                localStorage.setItem('allNotes',JSON.stringify(notes));
+            }
+        }
+    }
+}else{
+            return;
+        }
     }else{
     notes.push({
             title: notesTitle,
@@ -82,6 +96,7 @@ function saved(event){
         newBtn.append(t);
         uniqueTitle.push(notesTitle);
         localStorage.setItem('uniqueTitle', JSON.stringify(uniqueTitle));
+        newBtn.addEventListener('click', openNote);
     }
 }
 
@@ -97,5 +112,38 @@ function renderNotes(){
         previewNotes.appendChild(newBtn);
         let t=document.createTextNode(notesTitle || "New Note");
         newBtn.append(t);
+        newBtn.addEventListener('click', openNote);
+    }
+}
+    function openNote(){
+    let savedNotes=JSON.parse(localStorage.getItem('allNotes'));
+    for(let i in savedNotes){
+        for(let key in savedNotes[i]){
+            if(savedNotes[i][key]==this.id){
+                let m=savedNotes[i][key];
+                let n=savedNotes[i]['content'];
+                document.getElementById('notesTitle').value=m;
+                document.getElementById('notesContent').value=n;
+            }
+        }
+    }
+}
+
+function newNote(){
+    let form=document.getElementById('notesInput');
+    form.reset();
+}
+function removeNote(){
+    let c=confirm(`Are you sure you want to delete "${notesTitle.value}"`);
+    if (c==true){
+    for(i in notes){
+        if(notes[i]['title']==notesTitle.value){
+            let x=notes.findIndex(notes=>notes.title==notesTitle.value);
+            notes=notes.filter((data, idx)=> idx !=x);
+            localStorage.setItem('allNotes', JSON.stringify(notes));
+            let a=document.getElementById(notesTitle.value);
+            a.remove();
+        }
+    }
     }
 }
