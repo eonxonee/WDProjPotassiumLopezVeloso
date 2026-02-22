18ed155b6888db958cfdfe7fd5db7daec0f486b2
Hannah-Malaika-Lopez <b2029hmblopez@pshs.edu.ph>
2026-02-22 11:29:24 +0800

    as

--- main files/main.js
+++ main files/main.js
@@ -4,19 +4,83 @@ let previewNotes=document.getElementById('previewNotes');
 //onload
 function loaded(){
     renderNotes();
+    clock();
+
+}
+
+//clock 
+function clock(){
+    const currentTime = new Date();
+    var hours= currentTime.getHours();
+    var minutes= currentTime.getMinutes();
+    var seconds= currentTime.getSeconds();
+    seconds=addZero(seconds);
+    minutes=addZero(minutes);
+    document.getElementById("time").innerHTML = hours +":"+ minutes +":"+ seconds;
+    setTimeout(clock,1000);
+
+    var newDay=localStorage.getItem("day");
+    var newDayCounter=localStorage.getItem("dayCounter");
+    var newMonth=localStorage.getItem("month");
+    var newYear=localStorage.getItem("year");
+    
+    if(newDay){document.getElementById("day").innerHTML=newDay;}
+    if(newDayCounter){document.getElementById("dayCounter").innerHTML=newDay;}
+    if(newMonth){document.getElementById("month").innerHTML=newMonth;}
+    if(newYear){document.getElementById("year").innerHTML=newYear;}
+
+    var day=localStorage.getItem("day") || 18;
+    var dayCounter=localStorage.getItem("dayCounter") || 1;
+    var month=localStorage.getItem("month") || 2;
+    var year=localStorage.getItem("year") || 2019;
+
+
+    if(minutes==19 && seconds==0){
+        var dayNum=Number(day);
+        var dayCounterNum=Number(dayCounter);
+        dayNum++
+        dayCounterNum++
+        document.getElementById("day").innerHTML=dayNum;
+        localStorage.setItem("day", dayNum.toString());
+        document.getElementById("dayCounter").innerHTML=dayCounterNum;
+        localStorage.setItem("dayCounter", dayCounterNum.toString());
+    }
+    if(dayNum==30){
+        dayNum=1;
+        document.getElementById("day").innerHTML=dayNum;
+        localStorage.setItem("day", dayNum.toString());
+
+        var monthNum=Number(month);
+        monthNum++
+        document.getElementById("month").innerHTML=monthNum;
+        localStorage.setItem("month", monthNum.toString());
+    }
+    if(monthNum==12){
+        monthNum=1;
+        var yearNum=Number(year);
+        yearNum++
+        document.getElementById("year").innerHTML=yearNum;
+        localStorage.setItem("year", yearNum.toString());
+    }
+}
+function addZero(i){
+    if(i<10){i="0"+i}
+    return i;
 }
 
 //footerfx
 function footerToggle(){
-    document.getElementById("toggleFooter").classList.toggle("open");
-    document.getElementById("footer").classList.toggle("reveal");
-    document.getElementById("mask").classList.toggle("cover");
+    document.getElementById("toggleFooter").classList.add("open");
+    document.getElementById("footer").classList.add("reveal");
+    document.getElementById("mask").classList.add("cover");
+    document.getElementById('datesNstuff').classList.add('show');
     
 }
 function footerClose(){
     document.getElementById("toggleFooter").classList.remove("open");
     document.getElementById("footer").classList.remove("reveal");
     document.getElementById("mask").classList.remove("cover");
+    document.getElementById('datesNstuff').classList.remove('show');
     document.getElementById("notes").classList.add("open");
 }
 
